ebc64d28036cf4e2cf3d201f0f32c741b0a0d2a6
Keaton Freude <freudek90@gmail.com>
2013-01-24 12:28:19 -0800

    Initial Commit

---
+++ ArenaMapEditor/ArenaMapEditor/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace ArenaMapEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pctTiles = new System.Windows.Forms.PictureBox();
+            this.imgListTiles = new System.Windows.Forms.ImageList(this.components);
+            this.pctMap = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnMap = new System.Windows.Forms.Button();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctTiles)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctMap)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.pctTiles);
+            this.groupBox1.Location = new System.Drawing.Point(1286, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(239, 430);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tiles";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 401);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Load Tiles";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pctTiles
+            // 
+            this.pctTiles.Location = new System.Drawing.Point(6, 19);
+            this.pctTiles.Name = "pctTiles";
+            this.pctTiles.Size = new System.Drawing.Size(224, 376);
+            this.pctTiles.TabIndex = 0;
+            this.pctTiles.TabStop = false;
+            this.pctTiles.Click += new System.EventHandler(this.pctTiles_Click);
+            this.pctTiles.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pctTiles_MouseClick);
+            // 
+            // imgListTiles
+            // 
+            this.imgListTiles.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgListTiles.ImageStream")));
+            this.imgListTiles.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgListTiles.Images.SetKeyName(0, "Desert1.png");
+            this.imgListTiles.Images.SetKeyName(1, "Desert2.png");
+            this.imgListTiles.Images.SetKeyName(2, "Desert3.png");
+            this.imgListTiles.Images.SetKeyName(3, "Desert4.png");
+            this.imgListTiles.Images.SetKeyName(4, "Desert5.png");
+            this.imgListTiles.Images.SetKeyName(5, "Desert6.png");
+            this.imgListTiles.Images.SetKeyName(6, "Desert7.png");
+            this.imgListTiles.Images.SetKeyName(7, "Desert8.png");
+            this.imgListTiles.Images.SetKeyName(8, "Desert9.png");
+            this.imgListTiles.Images.SetKeyName(9, "Desert10.png");
+            this.imgListTiles.Images.SetKeyName(10, "Desert11.png");
+            this.imgListTiles.Images.SetKeyName(11, "Desert12.png");
+            this.imgListTiles.Images.SetKeyName(12, "Desert13.png");
+            this.imgListTiles.Images.SetKeyName(13, "Desert14.png");
+            this.imgListTiles.Images.SetKeyName(14, "Desert15.png");
+            this.imgListTiles.Images.SetKeyName(15, "Desert16.png");
+            this.imgListTiles.Images.SetKeyName(16, "Desert17.png");
+            this.imgListTiles.Images.SetKeyName(17, "Desert18.png");
+            this.imgListTiles.Images.SetKeyName(18, "Desert19.png");
+            this.imgListTiles.Images.SetKeyName(19, "Desert20.png");
+            this.imgListTiles.Images.SetKeyName(20, "Desert21.png");
+            this.imgListTiles.Images.SetKeyName(21, "Desert22.png");
+            this.imgListTiles.Images.SetKeyName(22, "Desert23.png");
+            this.imgListTiles.Images.SetKeyName(23, "Desert24.png");
+            this.imgListTiles.Images.SetKeyName(24, "Desert25.png");
+            this.imgListTiles.Images.SetKeyName(25, "Desert26.png");
+            this.imgListTiles.Images.SetKeyName(26, "Desert27.png");
+            this.imgListTiles.Images.SetKeyName(27, "Desert28.png");
+            this.imgListTiles.Images.SetKeyName(28, "Desert29.png");
+            this.imgListTiles.Images.SetKeyName(29, "Desert30.png");
+            this.imgListTiles.Images.SetKeyName(30, "Desert31.png");
+            this.imgListTiles.Images.SetKeyName(31, "Desert32.png");
+            this.imgListTiles.Images.SetKeyName(32, "Desert33.png");
+            this.imgListTiles.Images.SetKeyName(33, "Desert34.png");
+            this.imgListTiles.Images.SetKeyName(34, "Desert35.png");
+            this.imgListTiles.Images.SetKeyName(35, "Desert36.png");
+            this.imgListTiles.Images.SetKeyName(36, "Desert37.png");
+            this.imgListTiles.Images.SetKeyName(37, "Desert38.png");
+            this.imgListTiles.Images.SetKeyName(38, "Desert39.png");
+            this.imgListTiles.Images.SetKeyName(39, "Desert40.png");
+            this.imgListTiles.Images.SetKeyName(40, "Desert41.png");
+            this.imgListTiles.Images.SetKeyName(41, "Desert42.png");
+            this.imgListTiles.Images.SetKeyName(42, "Desert43.png");
+            this.imgListTiles.Images.SetKeyName(43, "Desert44.png");
+            this.imgListTiles.Images.SetKeyName(44, "Desert45.png");
+            this.imgListTiles.Images.SetKeyName(45, "Desert46.png");
+            // 
+            // pctMap
+            // 
+            this.pctMap.Location = new System.Drawing.Point(0, 27);
+            this.pctMap.Name = "pctMap";
+            this.pctMap.Size = new System.Drawing.Size(1280, 720);
+            this.pctMap.TabIndex = 1;
+            this.pctMap.TabStop = false;
+            this.pctMap.Click += new System.EventHandler(this.pctMap_Click);
+            this.pctMap.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pctMap_MouseClick);
+            this.pctMap.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pctMap_MouseMove);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1904, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // btnMap
+            // 
+            this.btnMap.Location = new System.Drawing.Point(1286, 487);
+            this.btnMap.Name = "btnMap";
+            this.btnMap.Size = new System.Drawing.Size(75, 23);
+            this.btnMap.TabIndex = 3;
+            this.btnMap.Text = "New Map";
+            this.btnMap.UseVisualStyleBackColor = true;
+            this.btnMap.Click += new System.EventHandler(this.btnMap_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.btnMap);
+            this.Controls.Add(this.pctMap);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pctTiles)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctMap)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ImageList imgListTiles;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pctTiles;
+        private System.Windows.Forms.PictureBox pctMap;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button btnMap;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+    }
+}
+
---
+++ ArenaMapEditor/ArenaMapEditor/Form1.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ArenaMapEditor
+{
+    public partial class Form1 : Form
+    {
+        public int selected_id = -1;
+        Graphics mapGraphics;
+
+        Map map = new Map();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            tile_mappings.Add(1, 1);
+            tile_mappings.Add(3, 2);
+            tile_mappings.Add(4, 3);
+            tile_mappings.Add(5, 4);
+            tile_mappings.Add(7, 5);
+
+            tile_mappings.Add(8, 6);
+            tile_mappings.Add(9, 7);
+            tile_mappings.Add(10, 8);
+            tile_mappings.Add(11, 9);
+            tile_mappings.Add(12, 10);
+            tile_mappings.Add(13, 11);
+            tile_mappings.Add(14, 12);
+
+            tile_mappings.Add(15, 13);
+            tile_mappings.Add(16, 14);
+            tile_mappings.Add(17, 15);
+            tile_mappings.Add(18, 16);
+            tile_mappings.Add(19, 17);
+            tile_mappings.Add(20, 18);
+            tile_mappings.Add(21, 19);
+
+            tile_mappings.Add(23, 20);
+            tile_mappings.Add(24, 21);
+            tile_mappings.Add(25, 22);
+            tile_mappings.Add(26, 23);
+            tile_mappings.Add(27, 24);
+
+            tile_mappings.Add(30, 25);
+            tile_mappings.Add(31, 26);
+            tile_mappings.Add(32, 27);
+            tile_mappings.Add(33, 28);
+            tile_mappings.Add(34, 29);
+
+            tile_mappings.Add(37, 30);
+            tile_mappings.Add(41, 31);
+
+            tile_mappings.Add(44, 32);
+            tile_mappings.Add(45, 33);
+            tile_mappings.Add(47, 34);
+            tile_mappings.Add(48, 35);
+
+            tile_mappings.Add(50, 36);
+            tile_mappings.Add(51, 37);
+            tile_mappings.Add(52, 38);
+            tile_mappings.Add(53, 39);
+            tile_mappings.Add(54, 40);
+            tile_mappings.Add(55, 41);
+            tile_mappings.Add(56, 42);
+
+            tile_mappings.Add(57, 43);
+            tile_mappings.Add(58, 44);
+            tile_mappings.Add(62, 45);
+            tile_mappings.Add(63, 46);
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics graphics = pctTiles.CreateGraphics();
+
+            int y = 0;
+
+            //for (int i = 0; i < imgListTiles.Images.Count; i++)
+            //{
+            //    if (i == 1 || i == 5 || i == 21)
+            //        continue;
+
+
+
+            //    if (i % 7 == 0 && i != 0 && i != 5)
+            //        y += 32;
+            //    imgListTiles.Draw(graphics, new Point((i * 32) % 224, y), i);
+
+            //}
+
+            imgListTiles.Draw(graphics, new Point(0, 0), 0);
+            imgListTiles.Draw(graphics, new Point(64, 0), 1);
+            imgListTiles.Draw(graphics, new Point(96, 0), 2);
+            imgListTiles.Draw(graphics, new Point(128, 0), 3);
+            imgListTiles.Draw(graphics, new Point(192, 0), 4);
+
+            for (int i = 0; i < 7; ++i)
+            {
+                imgListTiles.Draw(graphics, new Point(i * 32, 32), i + 5);
+            }
+
+            for (int i = 0; i < 7; ++i)
+            {
+                imgListTiles.Draw(graphics, new Point(i * 32, 64), i + 12);
+            }
+
+            for (int i = 1; i < 6; ++i)
+            {
+                imgListTiles.Draw(graphics, new Point(i * 32, 96), i + 18);
+            }
+
+            for (int i = 1; i < 6; ++i)
+            {
+                imgListTiles.Draw(graphics, new Point(i * 32, 128), i + 23);
+            }
+
+            imgListTiles.Draw(graphics, new Point(32, 160), 29);
+            imgListTiles.Draw(graphics, new Point(160, 160), 30);
+
+            imgListTiles.Draw(graphics, new Point(32, 192), 31);
+            imgListTiles.Draw(graphics, new Point(64, 192), 32);
+
+            imgListTiles.Draw(graphics, new Point(128, 192), 33);
+            imgListTiles.Draw(graphics, new Point(160, 192), 34);
+
+            for (int i = 0; i < 7; ++i)
+            {
+                imgListTiles.Draw(graphics, new Point(i * 32, 224), 35 + i);
+            }
+
+            imgListTiles.Draw(graphics, new Point(0, 256), 42);
+            imgListTiles.Draw(graphics, new Point(32, 256), 43);
+
+            imgListTiles.Draw(graphics, new Point(160, 256), 44);
+            imgListTiles.Draw(graphics, new Point(192, 256), 45);
+
+        }
+
+        private void pctTiles_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        Dictionary<int, int> tile_mappings = new Dictionary<int, int>();
+
+        private void pctTiles_MouseClick(object sender, MouseEventArgs e)
+        {
+            /* Transform local coordinates into grid-coordinates */
+
+            int x = e.X / 32;
+            int y = e.Y / 32;
+
+            int test_index = (y * 7 + x) + 1;
+
+            
+
+            //this.Text = selected_id.ToString();
+
+            //map.map[x, y] = new Cell(selected_id, x * 32, y * 32);
+
+            if (tile_mappings.ContainsKey(test_index))
+                selected_id = tile_mappings[test_index];
+        }
+
+        private void btnMap_Click(object sender, EventArgs e)
+        {
+            mapGraphics = pctMap.CreateGraphics();
+
+            map.Draw(mapGraphics, imgListTiles);
+        }
+
+        private void pctMap_MouseClick(object sender, MouseEventArgs e)
+        {
+            int x = e.X / 32;
+            int y = e.Y / 32;
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                map.map[x, y] = new Cell(selected_id, x * 32, y * 32);
+
+                map.map[x, y].Draw(mapGraphics, imgListTiles);
+            }
+            else if (e.Button == System.Windows.Forms.MouseButtons.Right)
+            {
+                map.map[x, y] = new Cell(27, x * 32, y * 32);
+
+                map.map[x, y].Draw(mapGraphics, imgListTiles);
+            }
+
+        }
+
+        private void pctMap_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pctMap_MouseMove(object sender, MouseEventArgs e)
+        {
+            int x = e.X / 32;
+            int y = e.Y / 32;
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                if (selected_id >= 0)
+                {
+                    map.map[x, y] = new Cell(selected_id, x * 32, y * 32);
+
+                    map.map[x, y].Draw(mapGraphics, imgListTiles);
+                }
+            }
+            else if (e.Button == System.Windows.Forms.MouseButtons.Right)
+            {
+                map.map[x, y] = new Cell(27, x * 32, y * 32);
+
+                map.map[x, y].Draw(mapGraphics, imgListTiles);
+
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            /* Save current map out to file, ask for name */
+
+            SaveFileDialog dlg = new SaveFileDialog();
+
+            dlg.ShowDialog();
+
+            map.WriteToFile(dlg.FileName);
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            mapGraphics = pctMap.CreateGraphics();
+            dlg.ShowDialog();
+
+            map.OpenFromFile(dlg.FileName);
+
+            map.Draw(mapGraphics, imgListTiles);
+        }
+    }
+
+    public class Cell
+    {
+        public int cell_id;
+        public int x, y;
+
+        public Cell(int cell_id, int x, int y)
+        {
+            this.cell_id = cell_id;
+            this.x = x;
+            this.y = y;
+        }
+
+        public void Draw(Graphics g, ImageList imgList)
+        {
+            imgList.Draw(g, new Point(x, y), cell_id - 1);
+        }
+        
+    }
+
+    public class Map
+    {
+        public Cell[,] map;
+
+        public const int MAP_WIDTH = 40;
+        public const int MAP_HEIGHT = 22;
+
+        public Map()
+        {
+            map = new Cell[MAP_WIDTH, MAP_HEIGHT];
+
+            for (int i = 0; i < 40; ++i)
+            {
+                for (int j = 0; j < 22; ++j)
+                {
+                    map[i,j] = new Cell(27, i * 32, j * 32);
+                }
+            }
+        }
+
+        public void Draw(Graphics g, ImageList imgList)
+        {
+            for (int i = 0; i < 40; ++i)
+            {
+                for (int j = 0; j < 22; ++j)
+                {
+                    map[i,j].Draw(g, imgList);
+                }
+            }
+        }
+
+        public void WriteToFile(string filename)
+        {
+            System.IO.StreamWriter writer = new System.IO.StreamWriter(filename);
+
+            for (int i = 0; i < 40; ++i)
+            {
+                for (int j = 0; j < 22; ++j)
+                {
+                    if (i == 39 && j == 21)
+                        writer.Write(map[i, j].cell_id);
+                    else
+                        writer.Write(map[i, j].cell_id + ",");
+                }
+            }
+
+            writer.Close();
+        }
+
+        public void OpenFromFile(string filename)
+        {
+            System.IO.StreamReader reader = new System.IO.StreamReader(filename);
+
+            string[] broken = reader.ReadToEnd().Split(',');
+
+            int count = 0;
+
+            for (int i = 0; i < MAP_WIDTH; ++i)
+            {
+                for (int j = 0; j < MAP_HEIGHT; ++j)
+                {
+                    map[i, j].cell_id = Convert.ToInt32(broken[count]);
+                    count++;
+                }
+            }
+
+            reader.Close();
+           
+        }
+    }
+}
